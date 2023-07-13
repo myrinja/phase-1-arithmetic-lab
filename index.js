fda5364bc54365c9585253d771172946437e6c67
myrinja <myrinjau98@gmail.com>
2023-07-13 15:48:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -2,23 +2,19 @@
 // Write your code here
 let num1= 31;
 let num2= 2;
-if (typeof num1 === 'number' && typeof num2 === 'number'){
+{
   let multiply = num1 * num2;
 }
 let random =9;
  Math.random ();
 
- let num3 =8;
- let num4 =2;
-  
-   let mod =num3 / num4;
+ let num3 =39;
+ let num4 =5;
+
+   let mod =num3 % num4;
  
-   let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
-//let random = 9;
+   let max = Math.max(10,11,12,13,14,15,16,17,18,19,20);
 
-//Math.random(){
-
-//}
 
 
 
